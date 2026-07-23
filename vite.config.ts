bb3adeeb0a95b383f0c80c32487b992180a3dd37
Dacxi442 <wegadaniel861@gmail.com>
2026-07-23 15:00:08 +0300

    Migrate from tanstack to react

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import path from "path";
 
 export default defineConfig({
-  plugins: [
-    tanstackStart({
-      server: {
-        entry: "server",
-      },
-    }),
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
-    tsconfigPaths: true,
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   server: {
     port: 5173,
